ba2dac674b22e97c00f2f2c2db4ffcec5c2cb070
zy <503636805@qq.com>
2018-05-08 21:27:25 +0800

    登陆界面初始化完成

---
+++ src/components/login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="login-container">
+      <h3>{{msg}}</h3>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: '登陆界面开始'
+    }
+  }
+}
+</script>
+<style>
+h3 {
+  font-weight: 700;
+  font-size: 25px;
+  text-align: center;
+  color: red;
+}
+</style>
